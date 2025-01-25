a583a9066bc4b6e4829bf0bf0c6fa45d1107c1cb
RkJaiswal3 <rohitkrj929@gmail.com>
2025-01-25 14:32:50 +0545

    bug fixes in projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,6 +1,7 @@
 function Projects() {
   const projectList = [
     {
+      webLink: "https://rkjaiswal3.github.io/textFormatingTool/",
       title: "Text-Formating Tool",
       description:
         "An online platform of formating Text.TextFormating Tool from which you can formatting the text or manipulate the text as per your demand.",
@@ -8,6 +9,7 @@ function Projects() {
       repo: "https://github.com/RkJaiswal3/textFormatingTool?tab=readme-ov-file",
     },
     {
+      webLink: "https://newstadka.vercel.app",
       title: "NewsTadka.COM",
       description:
         "This is an online platform to get news of real world at real Time. In it I used the news API in it.",
@@ -15,6 +17,7 @@ function Projects() {
       repo: "https://github.com/RkJaiswal3/NewsTadka",
     },
     {
+      webLink: "https://ibook-six.vercel.app",
       title: "IBOOK",
       description:
         "This is the mern base project. In it we can add note and we can modify it by adding and updating our notes.  ",
@@ -39,6 +42,14 @@ function Projects() {
               <div className="card-body">
                 <h5 className="card-title">{project.title}</h5>
                 <p className="card-text">{project.description}</p>
+
+                <a
+                  href={project.webLink}
+                  style={{ textDecoration: "none" }}
+                  target="_blank"
+                >
+                  Visit Site
+                </a>
               </div>
               <div className="card-footer text-center">
                 {/* <a
