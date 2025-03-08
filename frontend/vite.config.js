17b444ca882be388451c031d689432a3c4d3f0d3
RkJaiswal3 <rohitkrj929@gmail.com>
2025-03-08 11:27:52 +0545

    vercel.json file changed

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,14 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: "./",  // Ensures correct file paths
   build: {
-    outDir: "dist"  // Ensure output directory is set correctly
+    outDir: "dist"
   }
 });
